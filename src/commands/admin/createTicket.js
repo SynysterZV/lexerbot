e4e2587fe5b4a79305ac846d34b6e8ebc0010667
SynysterZV <greendayfanj@gmail.com>
2021-01-06 09:25:42 -0500

    Changed perms

--- src/commands/admin/createTicket.js
+++ src/commands/admin/createTicket.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     execute(message, args) {
     if(message.mentions.members.first()) {
-        if(!message.member.permissions.has(['ADMINISTRATOR'])) return message.channel.send('You need to be an administrator to make a ticket for somebody else!')
+        if(!message.member.permissions.has(['MANAGE_GUILD'])) return message.channel.send('You need to be an administrator to make a ticket for somebody else!')
         args.single()
     }
     message.guild.channels.createTicket(lexure.joinTokens(args.many()), message, { parent: message.channel.parent });
